a601cd323aa167b57379b33115bce2b30ccec089
benedictussteven <49013736+benedictussteven@users.noreply.github.com>
2021-09-12 14:07:39 +0700

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ function Welcome(props) {
       <h1>Nama Saya {props.name}</h1>
       <p>NIM saya {props.nim}</p>
       <ul>
-          {props.namaHobby.map((post) =>
+          {props.hobby.map((post) =>
             <li key={post.id}>
               <b>{post.title}</b> {post.content}
             </li>
@@ -18,20 +18,20 @@ function Welcome(props) {
 
 class Home extends Component {
     render() {
-        const namaHobby = [
+        const hobby = [
             {id: 1, title: 'Main Games', content: 'Sangat membantu dalam menaikan mood '},
             {id: 2, title: 'Baca Komik', content: 'Baik dalam mengisi waktu luang'}
           ];
         return (
             <div>
-                <p> Anggota Kelompok 5</p>
+                <p> Anggota Kelompok 5</p> 
                 <hr/>
                 <Fathur 
                     title = "Fathur Rahman Ar Rasyid" 
                     nim = "21120118140050"
                     asal = "Jakarta" />
                 <hr/>
-                <Welcome name="Benecditus Steven" nim="21120118130057"  namaHobby={namaHobby}/>
+                <Welcome name="Benecditus Steven" nim="21120118130057"  hobby={hobby}/>
             </div>
  
         )
